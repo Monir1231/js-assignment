b7e2b739c3617fdaab61416795d58249592a4f15
Monir1231 <monirmolla324@gmail.com>
2025-06-17 18:41:55 +0600

    js poblem solve

--- poblem-1/script.js
+++ poblem-1/script.js
@@ -47,8 +47,41 @@ x = x * y ;
 
 // 6.Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").
 
-const person = {
-    name : "John",
-    age :50 
-}
-alert(`${person.name}  is  ${person.age}`)
+// const person = {
+//     name : "John",
+//     age :50 
+// }
+// alert(`${person.name}  is  ${person.age}`)
+
+// 7. The <button> element should do something when someone clicks on it. Try to fix it!
+
+// function myfun(){
+//     alert("Hello hublu family")
+// }
+
+// 8.Array Related Question 
+
+// 1. Alert the number of items in an array, using the correct Array property: 
+
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+
+// alert(` total item is ${cars.length}`)
+
+// 2. Change the first item of Brand to "Ford".
+
+// const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+// Brand[0] = "Ford"
+// console.log(Brand)
+
+
+// 9.Math Related Problems
+
+// 1. Use the correct Math method to create a random number.
+
+// let myrandeom = Math.random()
+// console.log(myrandeom)
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+
+let myMax = Math.max(10,20)
+console.log(myMax)
