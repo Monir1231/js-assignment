850920a8c21b399fc1169a84e39d8815542d244e
Monir1231 <monirmolla324@gmail.com>
2025-06-17 11:34:33 +0600

    js solve

---
+++ poblem-1/script.js
@@ -0,0 +1,54 @@
+// 1. Create a variable called `carName`, assign the value `Volvo` to it.
+
+// let carName = "Volvo"
+
+// 2.On one single line, declare three variables with the following names and values:
+
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 3rd Variable Name And Values Both! 
+
+// let firstName = "John";  
+// let lastName = "Doe" 
+// let age = 35 ; 
+
+
+
+// 3.Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+// Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+x = 5 ;
+y = 10
+x = x * y ;
+// console.log(x)
+
+// 4.Use comments to describe the correct data type of the following variables:
+
+// Use comments to describe the correct data type of the following variables:
+
+// let length = 16; // number
+// let lastName = "Johnson"; // string
+
+// const x = {
+//   firstName: "John",  
+//   lastName: "Doe"
+// };    // Object
+
+
+// 5.Execute the function named myFunction.
+
+//  function myFunction() {
+//   alert("Hello World!");
+// }
+// myFunction()
+
+// 6.Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").
+
+const person = {
+    name : "John",
+    age :50 
+}
+alert(`${person.name}  is  ${person.age}`)
